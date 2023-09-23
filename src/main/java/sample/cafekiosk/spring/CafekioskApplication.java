defb307b2e4adba0130aada39a482d5b91d0dfa4
JinSung-Hwang <wlstjd7782@gmail.com>
2023-09-23 22:08:19 +0900

    feature: 섹션5 - persistence layer 테스트1

--- src/main/java/sample/cafekiosk/spring/CafekioskApplication.java
+++ src/main/java/sample/cafekiosk/spring/CafekioskApplication.java
@@ -2,7 +2,9 @@ package sample.cafekiosk.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA를 통해 데이터베이스와 상호작용하는 동안 엔터티의 생성 및 수정 시간을 자동으로 기록 할 수 있음, 이것은 @EntityListeners이거와 같이 사용함
 @SpringBootApplication
 public class CafekioskApplication {
 
