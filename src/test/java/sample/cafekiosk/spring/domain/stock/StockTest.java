3e363fc471e71e8347bac93b64c284bf8192f9bd
JinSung-Hwang <wlstjd7782@gmail.com>
2023-10-07 12:08:56 +0900

    feature: 섹션7 - 더 나은 테스트를 작성하기 위한 조언 - @DynamicTest

--- src/test/java/sample/cafekiosk/spring/domain/stock/StockTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/stock/StockTest.java
@@ -1,7 +1,11 @@
 package sample.cafekiosk.spring.domain.stock;
 
+import java.util.Collection;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -48,6 +52,52 @@ class StockTest {
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("차감할 재고 수량이 없습니다.");
   }
+
+   // note: 테스트가 시나리오적으로 단계 단계 검증하면서 테스트하고 싶을때 DynamicTest를 진행할 수 있다.
+  @DisplayName("")
+  @TestFactory
+  Collection<DynamicTest> dynamicTest() {
+    return List.of( // note: 이터러블한 객체를 리턴하면 된다. list, stream 등등
+        DynamicTest.dynamicTest("", () -> {
+
+        }),
+        DynamicTest.dynamicTest("", () -> {
+
+        })
+    );
+  }
+
+  //note: 다이나믹 테스트 예시
+  @DisplayName("재고 차감 시나리오")
+  @TestFactory
+  Collection<DynamicTest> stockDeductionDynamicTest() {
+    // given
+    Stock stock = Stock.create("001", 1);
+
+    return List.of(
+        DynamicTest.dynamicTest("재고를 주어진 만큼 차감할 수 있다.", () -> {
+          // given
+          int quantity = 1;
+
+          // when
+          stock.deductQuantity(quantity);
+
+          // then
+          assertThat(stock.getQuantity()).isZero();
+        }),
+        DynamicTest.dynamicTest("재고보다 많은 재고를 차감하려고하면 예외가 발생한다.", () -> {
+          // given
+          int quantity = 1;
+
+          // then
+          assertThatThrownBy(() -> stock.deductQuantity(quantity))
+              .isInstanceOf(IllegalArgumentException.class)
+              .hasMessage("차감할 재고 수량이 없습니다.");
+        })
+    );
+  }
+
+
 
 
 }
